713e2ca21cd9e4c36c56423c138077e511a32d75
QChown <132530931+QChown@users.noreply.github.com>
2023-12-14 21:37:39 -0700

    added notFound page

---
+++ app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NotFound() {
+  return (
+    <main className='not-found'>
+      <h1>404</h1>
+      <p>Unfortunatley, we could not find the requested page or resource.</p>
+    </main>
+  );
+}
+
+export default NotFound;
